6da3b0a96d9b3646aceee41c144d6df2ef730e9f
yabluko <yurarobak7@gmail.com>
2023-06-28 22:17:25 +0300

    async await and promises

--- index.js
+++ index.js
@@ -1,191 +1,70 @@
 
-// const div = document.createElement('div');
 
-
-// div.classList.add('wrapper');
-
-
-// const body = document.body ;
-
-// body.appendChild(div);
-
-
-// const header = document.createElement('h1');
-// header.textContent = "DOM (Document Object Model)";
-
-// div.insertAdjacentElement('beforebegin', header);
-
-
-// const ul = `
-//     <ul>
-//     <li>один</li>
-//     <li>два</li>
-//     <li>три</li>
-//     </ul>
-// `;
-
-
-
-
-// div.innerHTML = ul;
-
-// // =================================================
-
-
-// const img = document.createElement('img');
-
-
-
-// img.src = 'https://picsum.photos/200';
-
-// img.width = 240;
-
-// img.classList.add('fucker');
-
-// img.alt = 'Syper man';
-
-// div.appendChild(img);
-
-// const div2 = `
-//     <div class='pDiv'>
-//     <p>Параграф 1</p>
-//     <p>Параграф 2</p>
-//     </div>
-// `;
-
-// const ulElement = div.querySelector('ul');
-// ulElement.insertAdjacentHTML("afterbegin", div2);
-
-// const pdiv = document.querySelector('.pDiv');
-
-// pdiv.children[1].classList.add('text');
-
-
-// pdiv.firstElementChild.remove();
-
-
-
-// const generateAutoCard = (brand , color, year) => {
-//     const data = new Date;
-//     const curryear = data.getFullYear();
-//     return  `
-//     <div class="autoCard">
-//     <h2>${brand} ${year}</h2>
-//     <p>Auto ${brand} - ${year} year. Age of auto - ${curryear - year} years.</p>
-//     <p>Color: ${color}</p>
-//     <button type="button" class="btn">Delete</button>
-//     </div>
-//     `;
-   
-// }
-
-
-
-// const newdiv = document.createElement('div');
-
-// newdiv.classList.add('autos');
-
-
-// const carsList = [
-//     {brand: 'Tesla', year: 2015, color: 'Red'},
-//     {brand: 'Lexus', year: 2016, color: 'Silver'},
-//     {brand: 'Nissan', year: 2012, color: 'Black'},
-// ]
-
-
-// const cardsHTML = carsList.map((car) => {
-//     return generateAutoCard(car.brand, car.color, car.year);
-// }).join('')
-
-
-// newdiv.innerHTML = cardsHTML;
- 
-
-
-// div.insertAdjacentElement("beforebegin", newdiv);
-
-// const buttons = document.querySelectorAll('.btn');
-
-
-// function Deleting(e){
-//     const currentButton = e.currentTarget;
-//     currentButton.closest('.autoCard').remove();
-//     console.log(currentButton.parentElement);
-// }
-
-
-
-// buttons.forEach((button) =>{
-//     button.addEventListener("click", Deleting);
+// fetch('https://api.github.com/users/yabluko').then(res =>{
+//   return res.json();
+// }).then(res => {
+//   console.log(res);
+// }).catch(err => {
+//   console.log('Errorr >>', err);
 // })
 
-//силка на массив
-// const students = ['Oleg','Yurii','Veronika','Vasia'];
+// async function getGitData(){
+//   try{
 
-
-
-// const group2 = students.slice();
-// group2[0] = 'Sasha';
-
-
-
-// const group3 = [].concat(students);
-
-
-// const group4 = [...students];
-
-// const group5 = Array.from(students);
-
-
-
-// const person = {
-//     name :'Peter',
-//     age: 30 ,
-//     social: {
-//         twitter : 'twitter',
-//         facebook : 'facebook',
-//     }
+//     const response = await fetch('https://api.github.com/users/yabluko');
+//     const data = await response.json();
+//     console.log(data);
+//   } catch(err){
+//     console.log("Error ");
+//   }
 // }
 
-// const student = person;
-// student.age = 100;
+// getGitData();
+const video = document.querySelector('video');
 
-// console.log(person);
+// const myVideo = navigator.mediaDevices.getUserMedia({video : true}).then(mediaStream => {
+//   video.srcObject = mediaStream;
+// });
+// console.log(myVideo);
 
+// async function getUserVideo () {
+//   const response = await navigator.mediaDevices.getUserMedia({video: true});
+//   video.scrObject = response;
+// }
 
+// getUserVideo();
 
-// const policeman = Object.assign({}, person, {skills: 'good communication'});
+// function sleep(time){
+//   return new Promise((resolve, reject) => {
+//     if (time < 500){
+//       reject('To few sleeping time');
+//     }
+//     setTimeout(() => resolve(`I was sleeping ${time}`), time );
+//   });
+// }
 
+// sleep(1500).then(res => {
+//   console.log(res);
+//   return sleep(1000)
+// }).then(res => {
+//   console.log(res);
+//   return sleep(500);
+// }).then(res => {
+//   console.log(res);
+//   return sleep(200);
+// }).then(res => {
+//   console.log(res);
+// }).catch( )
+// let b = 30;
 
+// console.log(b);
 
-// const newperson = JSON.parse(JSON.stringify(person));
-// newperson.social.twitter = 'kekeke1111';
+// function sleep(b){
+//   return new Promise((resolve, reject) => {
+//     setTimeout (resolve(`Next numer is - ${b = 40}`) ,3000);
+//   });
+// }
 
-
-
-// console.log(newperson);
-
-
-
-
-const people = [
-    { name: 'Stas', year: 2000 },
-    { name: 'Vasya', year: 1986 },
-    { name: 'Alex', year: 1990 },
-    { name: 'Oleg', year: 2004 },
-  ];
-
-  const comments = [
-    { text: 'Good post!', id: 523423 },
-    { text: 'I liked it', id: 823423 },
-    { text: 'Keep going bro', id: 2039842 },
-    { text: 'Trash!!!!', id: 123523 },
-    { text: 'Normally', id: 542328 }
-  ];
-
-  // Check that at least one person has 16years old
-
-  const sixteenYears = people.some(person => new Data().getFullYear() - person.year >= 16);
-
-  console.log(sixteenYears);
-
+// sleep(b).then(res =>  {
+//   console.log(res);
+// })
